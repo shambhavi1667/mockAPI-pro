5a606139e186aa9228ea5bef542c25fe5d8de0cb
shambhavi kulkarni <shambhaviik18@gmail.com>
2026-03-07 22:42:47 +0530

    use catch-all mock route

--- src/routes/mockRoutes.js
+++ src/routes/mockRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mockController = require("../controllers/mockController");
 
-// Catch all dynamic routes
-router.all("/:projectId/:endpoint(*)", mockController.handleMockRequest);
+// Catch all requests under /mock
+router.all("*", mockController.handleMockRequest);
 
 module.exports = router;
