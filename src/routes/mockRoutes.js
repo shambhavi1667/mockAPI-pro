1d58c8e7577a3deb25782fa33bf3d7d28417a2bc
shambhavi kulkarni <shambhaviik18@gmail.com>
2026-03-07 22:56:00 +0530

    fix express wildcard route crash

--- src/routes/mockRoutes.js
+++ src/routes/mockRoutes.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mockController = require("../controllers/mockController");
 
-// catch everything under /mock/:projectId/...
-router.all("/:projectId/*", (req, res, next) => {
-  console.log("Mock route hit:", req.originalUrl);
-  next();
-}, mockController.handleMockRequest);
+// Dynamic mock route
+router.all("/:projectId/:endpoint", mockController.handleMockRequest);
 
 module.exports = router;
