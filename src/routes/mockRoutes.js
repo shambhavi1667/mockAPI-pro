6a7ac11f7b5ecdd520c9335ba825dda787a4ced9
shambhavi kulkarni <shambhaviik18@gmail.com>
2026-03-11 00:45:51 +0530

    fix: wildcard route syntax

--- src/routes/mockRoutes.js
+++ src/routes/mockRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const mockController = require("../controllers/mockController");
 
 // Catch all routes under /mock
-router.all("/*", mockController.handleMockRequest);
+router.all("/*path", mockController.handleMockRequest);
 
 module.exports = router;
